b248859a76122f7a546065e9fa2b87947bdf4cc1
Isaac <gr25.main@gmail.com>
2022-02-13 20:52:04 +0000

    shuffle

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { cardList } from "./card-list.js";
 function main() {
 
     const elementAreas = ['swap-area', 'special-area', 'stack-area'];
+    let allCards = [];
 
     function generateGame() {
         elementAreas.forEach(element => {
@@ -13,7 +14,31 @@ function main() {
                 children[i].className = cardList.symbol0.className;
             }
         });
+        allCards.push(new card(cardList.symbolS.symbol, cardList.symbolS.className));
+        for (let i = 0; i < 4; i++) {
+            allCards.push(new card(cardList.symbol1.symbol, cardList.symbol1.className));
+            allCards.push(new card(cardList.symbol2.symbol, cardList.symbol2.className));
+            allCards.push(new card(cardList.symbol3.symbol, cardList.symbol3.className));
+        }
+        for (let i = 1; i < 10; i++) {
+            allCards.push(new card(i.toString(), cardList.symbol1.className));
+            allCards.push(new card(i.toString(), cardList.symbol2.className));
+            allCards.push(new card(i.toString(), cardList.symbol3.className));
+        }
+        shuffle();
     }
+
+    function shuffle() {
+        for (let i = 0; i < 1000; i++) {
+            const x = getRandomInt(0, allCards.length);
+            const y = getRandomInt(0, allCards.length);
+            const z = [allCards[x], allCards[y]];
+            allCards[x] = z[1];
+            allCards[y] = z[0];
+        }
+    }
+
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min; // inc min, exc max
 
     generateGame();
 }
