1ade235094f092dbfc4b654dd61c49993bf51dbd
Isaac <gr25.main@gmail.com>
2022-02-13 02:46:06 +0000

    style + generate top

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 function main() {
 
+    const symbol0 = '▯';
+    const symbol1 = '#';
+    const symbol2 = '&';
+    const symbol3 = '$';
+    const symbolS = '%';
+
+    const elementAreas = ['swap-area', 'special-area', 'stack-area'];
+
+    function generateGame() {
+        elementAreas.forEach(element => {
+            const children = document.getElementsByClassName(element)[0].children;
+            for (let i = 0; i < children.length; i++) {
+                children[i].innerHTML = symbol0;
+                children[i].className = 'blank';
+            }
+        });
+    }
+
+    generateGame();
 }
 
 window.addEventListener('load', main);
