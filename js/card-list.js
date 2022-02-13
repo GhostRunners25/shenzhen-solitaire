4921a05e013a1087fad9e294af0c23868b85f56a
Isaac <gr25.main@gmail.com>
2022-02-13 12:48:44 +0000

    card-list

---
+++ js/card-list.js
@@ -0,0 +1,22 @@
+export const cardList = {
+    symbol0: {
+        symbol: '▯',
+        className: 'blank',
+    },
+    symbol1: {
+        symbol: '#',
+        className: 'red',
+    },
+    symbol2: {
+        symbol: '&',
+        className: 'blue',
+    },
+    symbol3: {
+        symbol: '$',
+        className: 'green',
+    },
+    symbolS: {
+        symbol: '%',
+        className: 'special'
+    },
+}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 import card from "./card.js";
+import { cardList } from "./card-list.js";
 
 function main() {
-
-    const symbol0 = '▯';
-    const symbol1 = '#';
-    const symbol2 = '&';
-    const symbol3 = '$';
-    const symbolS = '%';
 
     const elementAreas = ['swap-area', 'special-area', 'stack-area'];
 
@@ -14,8 +9,8 @@ function main() {
         elementAreas.forEach(element => {
             const children = document.getElementsByClassName(element)[0].children;
             for (let i = 0; i < children.length; i++) {
-                children[i].innerHTML = symbol0;
-                children[i].className = 'blank';
+                children[i].innerHTML = cardList.symbol0.symbol;
+                children[i].className = cardList.symbol0.className;
             }
         });
     }
